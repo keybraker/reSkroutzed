3e2ebfaee85b8cd70c3d685cbaa7237ff1de7860
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-04-26 20:02:24 +0300

    Update frequently bought together ad

--- flaggers/frequentlyBoughtTogetherFlagger.js
+++ flaggers/frequentlyBoughtTogetherFlagger.js
@@ -1,68 +1,19 @@
 function frequentlyBoughtTogetherFlagger() {
-  const topSpanTexts = document.querySelectorAll(
-    "span:not(.flagged-bought-together)"
+  const divElements = document.querySelectorAll(
+    "div.fbt-content:not(.flagged-bought-together)"
   );
 
-  if (topSpanTexts.length === 0) {
-    // sponsoredCount = 0;
-  }
+  [...divElements].forEach((div) => {
+    const sponsoredSpan = div.querySelector("span.sp-tag");
 
-  [...topSpanTexts].filter(isSponsored).forEach((span) => {
-    flagSponsoredSpanMulti(span);
-    flagParentList(span);
-  });
-
-  const nonFlaggedListElements = document.querySelectorAll(
-    "ul:not(.flagged-bought-together)"
-  );
-
-  if (!nonFlaggedListElements || nonFlaggedListElements.length === 0) {
-    return;
-  }
-
-  processListElements(nonFlaggedListElements);
-}
-
-function processListElements(listElements) {
-  [...listElements].forEach((listElement) => {
-    const nonFlaggedSpanElements = listElement.querySelectorAll(
-      "span:not(.flagged-bought-together)"
-    );
-
-    if (nonFlaggedSpanElements) {
-      processSpanElements(nonFlaggedSpanElements, listElement);
+    if (sponsoredSpan && sponsoredSpan.textContent === "Sponsored") {
+      flagSponsoredSpanMulti(sponsoredSpan);
+      div.classList.add("flagged-bought-together");
     }
   });
-}
-
-function processSpanElements(spanElements, listElement) {
-  [...spanElements].filter(isSponsored).forEach((spanElement) => {
-    flagSponsoredSpan(spanElement);
-    flagParentList(listElement);
-
-    const sponsoredItems = listElement?.children[2]?.children[0]?.children;
-    if (sponsoredItems) {
-      [...sponsoredItems].forEach((sponsoredItem) =>
-        flagProductListItem(sponsoredItem)
-      );
-    }
-  });
-}
-
-function flagSponsoredSpan(spanElement) {
-  spanElement.classList.add("flagged-bought-together-label");
-  updateSponsoredText(spanElement, false);
 }
 
 function flagSponsoredSpanMulti(spanElement) {
   spanElement.classList.add("flagged-bought-together-label");
-  updateSponsoredText(spanElement, false);
-}
-
-function flagParentList(element) {
-  const parentDiv = element.parentDiv;
-  if (parentDiv) {
-    parentDiv.classList.add("flagged-bought-together");
-    toggleVisibility(parentDiv);
-  }
+  updateSponsoredText(spanElement, true);
 }
