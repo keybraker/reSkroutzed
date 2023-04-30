db864059dc26e2b1c8d5b563feffd802b02474de
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-05-01 02:17:57 +0300

    Fix error with shelf flagger counter being zero

--- src/flaggers/shelfFlagger.ts
+++ src/flaggers/shelfFlagger.ts
@@ -1,14 +1,22 @@
 import { State } from "../enums/State";
-import { isSponsored, updateSponsoredText } from "../helpers/helpers";
+import { isSponsored, toggleVisibility, updateSponsoredText } from "../helpers/helpers";
 import { flagProductListItem } from "./productFlagger";
 
 export function shelfFlagger(state: State): void {
-  const h4ElementsFlagged = document.querySelectorAll("h4.flagged-shelf");
-  state.sponsoredShelfCount = h4ElementsFlagged?.length ?? 0;
+  updateShelfCount(state);
 
   const h4Elements = document.querySelectorAll("h4:not(.flagged-shelf)");
 
   [...h4Elements].filter(isSponsored).forEach(element => updateShelfCountAndVisibility(element, state));
+}
+
+function updateShelfCount(state: State): void {
+  const flaggedShelf =
+    document.querySelectorAll("h4.flagged-shelf");
+
+  if (flaggedShelf?.length === 0) {
+    state.sponsoredShelfCount = 0;
+  }
 }
 
 function updateShelfCountAndVisibility(h4Element: Element, state: State): void {
@@ -18,13 +26,12 @@ function updateShelfCountAndVisibility(h4Element: Element, state: State): void {
   updateSponsoredText(h4Element, true, state);
 
   const h4ParentElement = h4Element.parentElement;
+
   if (h4ParentElement) {
+    h4Element.classList.add("flagged-shelf");
     h4ParentElement.classList.add("flagged-shelf");
 
-    const displayClass = "display-none";
-    state.visible
-      ? h4ParentElement.classList.remove(displayClass)
-      : h4ParentElement.classList.add(displayClass);
+    toggleVisibility(h4ParentElement, state);
 
     const sponsoredItems = h4ParentElement?.children[2]?.children[0]?.children;
 
