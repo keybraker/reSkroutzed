8b287f3d19bc4d3223c5a5babfccf7e19a59b36e
gtopsis <csd3004@csd.uoc.gr>
2023-11-25 08:50:24 +0200

    feat: check for invalid env param during extension build process

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,26 @@ const { merge } = require("webpack-merge");
 
 const commonConfig = require("./webpack/webpack.common.config");
 
-module.exports = (env) => {
-  const browser = env.chrome ? "chrome" : "firefox";
+const supportedBrowsersNames = ["chrome", "firefox"];
+const getBrowserName = (env) => {
+  const browserName = supportedBrowsersNames.find(
+    (supportedBrowserName) => env[supportedBrowserName] === true
+  );
 
-  const config = require(`./webpack/webpack.${browser}.config`);
+  return browserName;
+};
+
+module.exports = (env) => {
+  const browserName = getBrowserName(env);
+  if (!browserName) {
+    const supportedBrowsersList = supportedBrowsersNames.join(", ");
+
+    throw new Error(
+      `Parameter env should be one of the following values: ${supportedBrowsersList}`
+    );
+  }
+
+  const config = require(`./webpack/webpack.${browserName}.config`);
 
   return merge(commonConfig, config);
 };
