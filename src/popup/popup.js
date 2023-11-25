8e2581f33bcbf4599e36ad82ca0ec12cdc30263c
Ioannis Tsiakkas <keybraker@users.noreply.github.com>
2023-11-25 15:54:14 +0200

    Add hide action to mobile devices and new popup design (#22)

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,14 +1,12 @@
 function updateCounts(sponsored, sponsoredShelf, video) {
     document.getElementById("sponsoredCount").textContent = sponsored || 0;
-    document.getElementById("sponsoredShelfCount").textContent =
-    sponsoredShelf || 0;
+    document.getElementById("sponsoredShelfCount").textContent = sponsoredShelf || 0;
     document.getElementById("videoCount").textContent = video || 0;
 }
 
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (!tabs || tabs.length === 0) {
         updateCounts();
-
         return;
     }
 
