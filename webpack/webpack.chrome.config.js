2eaef486955bc057c946331fd50ada0bb7c96e65
Keybraker <iantsiakkas@gmail.com>
2025-04-17 22:06:42 +0300

    Move manifest out of source

--- webpack/webpack.chrome.config.js
+++ webpack/webpack.chrome.config.js
@@ -18,7 +18,7 @@ module.exports = {
         { from: "src/popup/popup.js", to: "popup.js" },
         { from: "src/popup/popup.html", to: "popup/popup.html" },
         { from: "src/popup/popup.css", to: "popup/popup.css" },
-        { from: "src/manifest_chrome.json", to: "manifest.json" },
+        { from: "manifests/manifest_chrome.json", to: "manifest.json" },
         { from: "src/assets/icons/*.png", to: "assets/icons/[name][ext]" },
         { from: "src/css/*.css", to: "css/[name][ext]" },
       ],
