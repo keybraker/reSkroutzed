b188cb81ce84515f622edede601abd87b6a9d35b
Keybraker <iantsiakkas@gmail.com>
2025-04-21 13:29:21 +0300

    Fix shelf handling

--- src/handlers/ShelfProductAd.handler.ts
+++ src/handlers/ShelfProductAd.handler.ts
@@ -3,17 +3,15 @@ import { State } from '../common/types/State.type';
 import { AdHandlerInterface } from './common/interfaces/adHandler.interface';
 
 export class ShelfProductAdHandler implements AdHandlerInterface {
-  private readonly shelfAdClass = ['sponsored-product-cards', 'sponsored-shelf'];
+  private readonly shelfAdClass = ['selected-product-cards', 'sponsored-shelf'];
   private readonly flaggedShelfAdClass = 'flagged-shelf';
 
   constructor(private state: State) {}
 
   public flag(): void {
-    // Reset the shelf ad count
     this.state.ShelfAdCount = 0;
 
     const allFlaggedShelfElements = DomClient.getElementsByClass(`.${this.flaggedShelfAdClass}`);
-    // Update ShelfAdCount with the number of flagged shelf elements
     this.state.ShelfAdCount = allFlaggedShelfElements.length;
 
     DomClient.getElementsByClass(`li:not(.${this.flaggedShelfAdClass})`).forEach((element) =>
@@ -36,8 +34,6 @@ export class ShelfProductAdHandler implements AdHandlerInterface {
       this.shelfAdClass.some((videoAdClass) => element.classList.contains(videoAdClass)) &&
       !element.classList.contains(this.flaggedShelfAdClass)
     ) {
-      this.state.productAdCount++;
-      // Increment ShelfAdCount when a new shelf ad is found
       this.state.ShelfAdCount++;
       DomClient.addClassesToElement(element, this.flaggedShelfAdClass);
       DomClient.updateElementVisibility(element, !this.state.hideShelfProductAds ? 'hide' : 'show');
@@ -46,8 +42,6 @@ export class ShelfProductAdHandler implements AdHandlerInterface {
 
   private flagElementsBySelector(selector: string): void {
     DomClient.getElementsByClass(selector).forEach((element) => {
-      this.state.productAdCount++;
-      // Increment ShelfAdCount when a new shelf ad is found
       this.state.ShelfAdCount++;
       DomClient.addClassesToElement(element, this.flaggedShelfAdClass);
       DomClient.updateElementVisibility(element, !this.state.hideShelfProductAds ? 'hide' : 'show');
